a2a5d155d1165cba1fb9cf3ae36d77aa3814f4ae
RohitShah1706 <rlshah03@gmail.com>
2022-04-16 22:47:42 +0530

    updated setInterval count

--- index.js
+++ index.js
@@ -194,7 +194,8 @@ function chooseRandomShapes() {
         sq.push(new shape([Math.random() * canvas.width, -Math.floor(Math.random() * 2 * canvas.height / 3)], 40, listShapesSize[Math.floor(Math.random() * 4)]));
     }
 }
-setInterval(chooseRandomShapes, 1000);
+chooseRandomShapes();
+setInterval(chooseRandomShapes, 4000);
 function background() {
     sq.forEach(square => {
         square.draw();
