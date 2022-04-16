210efb52ab28c4a929d1a2f21e4bc2cbaa6ce6e7
raj-bunsha <rajbunsha@gmail.com>
2022-04-16 22:26:05 +0530

    sparks

--- sparks/sparks.js
+++ sparks/sparks.js
@@ -1,45 +1,127 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext('2d');
+sparks = []
 
-class Ball {
-    constructor(x = canvas.width / 2, y = canvas.height - 20) {
-        this.position = { x: x, y: 3 * y / 5 }
-        this.vel = { x: 0, y: 0 };
-        this.rad = 20;
-        this.offset = 0;
+class Spark
+{
+    constructor(loc, angle, speed, color, scale=1)
+    {
+        this.loc=loc
+        this.angle=angle
+        this.speed=speed
+        this.scale=scale
+        this.color=color
+        this.alive=true
     }
-    draw() {
-        ctx.beginPath();
-        ctx.fillStyle = 'green';
-        ctx.arc(this.position.x, this.position.y - this.offset, this.rad, 0, Math.PI * 2);
-        ctx.fill();
-        ctx.closePath();
-    }
-    move() {
-        this.position.y += this.vel.y
-        this.position.x += this.vel.x
-        this.vel.y += 0.1
-        if (this.position.y < 3 * (canvas.height - 20) / 5) {
-            this.offset = this.position.y - 3 * (canvas.height - 20) / 5
+    point_towards(angle, rate)
+    {
+        rotate_direction = ((angle - this.angle + Math.PI * 3) % (Math.PI * 2)) - Math.PI
+        if(rotate_direction!=0)
+        {
+            rotate_sign = Math.abs(rotate_direction) / rotate_direction
         }
-        // this.collide()
+        if(Math.abs(rotate_direction) < rate)
+        {
+            this.angle=angle;
+        }
+        else
+        {
+            this.angle += rate * rotate_sign
+        }
     }
-    animate() {
 
+    calculate_movement(dt)
+    {
+        return [Math.cos(this.angle) * this.speed * dt, Math.sin(this.angle) * this.speed * dt]
     }
-    collide() {
-        if (this.position.y > canvas.height - this.rad) {
-            this.vel.y -= 0.1;
-            this.vel.y *= -1;
-            console.log(this.vel);
-            // console.log(this.vel.y);
+
+    // # gravity and friction
+    velocity_adjust(friction, force, terminal_velocity, dt)
+    {
+        var movement = this.calculate_movement(dt)
+        movement[1] = min(terminal_velocity, movement[1] + force * dt)
+        movement[0] *= friction
+        this.angle = Math.atan2(movement[1], movement[0])
+    }
+    //     # if you want to get more realistic, the speed should be adjusted here
+
+    move(dt)
+    {
+        var movement = this.calculate_movement(dt)
+        this.loc[0] += movement[0]
+        this.loc[1] += movement[1]
+        this.speed -= 0.1
+
+        if(this.speed <= 0)
+        {
+            this.alive=false;
+        }
+    }
+
+    //     # a bunch of options to mess around with relating to angles...
+    //     #this.point_towards(Math.PI / 2, 0.02)
+    //     #this.velocity_adjust(0.975, 0.2, 8, dt)
+    //     #this.angle += 0.1
+
+
+    draw(offset=[0, 0])
+    {
+        var points=[]
+        if(this.alive)
+        {
+            var points = [
+                [this.loc[0] + Math.cos(this.angle) * this.speed * this.scale, this.loc[1] + Math.sin(this.angle) * this.speed * this.scale],
+                [this.loc[0] + Math.cos(this.angle + Math.PI / 2) * this.speed * this.scale * 0.3, this.loc[1] + Math.sin(this.angle + Math.PI / 2) * this.speed * this.scale * 0.3],
+                [this.loc[0] - Math.cos(this.angle) * this.speed * this.scale * 3.5, this.loc[1] - Math.sin(this.angle) * this.speed * this.scale * 3.5],
+                [this.loc[0] + Math.cos(this.angle - Math.PI / 2) * this.speed * this.scale * 0.3, this.loc[1] - Math.sin(this.angle + Math.PI / 2) * this.speed * this.scale * 0.3],
+                ]
+                ctx.fillStyle=this.color
+                ctx.beginPath()
+                ctx.moveTo(points[0][0],points[0][1])
+                for (let i = 1; i < points.length; i++) {
+                    ctx.lineTo(points[i][0],points[i][1])
+                }
+                ctx.closePath();
+                ctx.stroke();
         }
     }
 }
-ball = new Ball();
+var sparks=[]
+// for (let i = 0; i < 50;i++) {
+//     sparks.push(new Spark([POSX,POSY],Math.random()*2*Math.PI,Math.random()*3+3,"#000",2))
+// }
 function animate()
 {
+    ctx.clearRect(0,0,canvas.width,canvas.height)
     requestAnimationFrame(animate);
-    ball.draw();
+    sparks.push(new Spark([POSX,POSY],Math.random()*2*Math.PI,Math.random()*2+2,"#000",1))
+    sparks.sort()
+    sparks.reverse()
+    console.log(sparks.length);
+    for (let i = 0; i < sparks.length; i++) {
+        sparks[i].move(1)
+            
+        sparks[i].draw()
+        if (!sparks[i].alive)
+        {
+            // sparks.pop(i)
+            // sparks.push(new Spark([POSX,POSY],Math.random()*2*Math.PI,Math.random()*3+3,"#000",1))
+        }
+    }
+    if(sparks.length>500)
+    {
+        sparks=sparks.slice(250,)
+    }
 }
-animate();
+var POSX=100;
+var POSY=100;
+// canvas.addEventListener("mousemove", function (e) {
+//     var a = getMousePosition(canvas, e);
+      
+// });
+// function getMousePosition(canvas, event) {
+//     let rect = canvas.getBoundingClientRect();
+//     POSX = event.clientX - rect.left;
+//     POSY = event.clientY - rect.top;
+// }
+animate()
